1c5a5317f84c85bcb3ca8a34854b3a440aebd1d2
Fernanda Coutinho <fernanda.coutinho@estudante.ufjf.br>
2025-09-18 22:29:04 -0300

    :sparkles:add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ufjf-dcc207-fernanda/ufjf-dcc207-2025-3-a-atv01-fernandacoutinho-1",
   plugins: [react()],
 })
